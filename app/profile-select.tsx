bd62ccbf203b2b4fefa2fa29ad45271acdd63266
Carlos Escamilla <carlosaescamilla3@gmail.com>
2026-01-12 13:30:47 -0800

    styling ProfileScreen

--- app/profile-select.tsx
+++ app/profile-select.tsx
@@ -1,30 +1,44 @@
-import { useAuth } from '@/context/auth-context';
-import { StyleSheet, Text, View } from 'react-native';
-import ProfileCard from './components/ProfileCard';
+import { useAuth } from "@/context/auth-context";
+import { StyleSheet, Text, View } from "react-native";
+import ProfileCard from "./components/ProfileCard";
 
-export default function ProfileSelectScreen () {
-    const {profiles} = useAuth();
+export default function ProfileSelectScreen() {
+  const { profiles } = useAuth();
 
-    function onPress(id: string) {
+  function onPress(id: string) {}
 
-    }
-
-    return (
-        <View style={styles.profilesContainer}>
-            <Text>Who is using Fridge Mate?</Text>
-            {
-                profiles.map((profile) => (
-                    <ProfileCard key={profile.profile_id} profile={profile}/>
-                ))
-            }
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Who is using Fridge Mate?</Text>
+      <View style={styles.profileContainer}>
+        {profiles.map((profile) => (
+          <ProfileCard key={profile.profile_id} profile={profile} />
+        ))}
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    profilesContainer: {
+    container: {
         flex: 1,
+        backgroundColor: "#141414",
         justifyContent: "center",
-    }
-})
+        alignItems: "center",
+        padding: 20
+    },
 
+    title: {
+        fontSize: 32,
+        color: "#fff",
+        marginBottom: 40,
+        fontWeight: "500"
+    },
+
+    profileContainer: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        gap: 20
+    }
+});
