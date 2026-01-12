514b3a4c6d3baf0ee5a44d5f0edc99eba995c019
Carlos Escamilla <carlosaescamilla3@gmail.com>
2026-01-12 13:23:12 -0800

    styling profileCard component

--- app/components/ProfileCard.tsx
+++ app/components/ProfileCard.tsx
@@ -1,5 +1,5 @@
 import { Profile } from "@/context/auth-context";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 interface ProfileCardProps {
     profile: Profile;
@@ -8,10 +8,45 @@ interface ProfileCardProps {
 
 export default function ProfileCard ({profile}: ProfileCardProps) {
     return (
-        <View>
-            <Text>{profile.first_name}</Text>
-            <Text>{profile.last_name}</Text>
-        </View>
+        <TouchableOpacity style={styles.card}>
+            <View style={styles.defaultAvatar}>
+                <Text style={styles.avatarText}>
+                    {profile.first_name.charAt(0).toUpperCase()}
+                </Text>
+            </View>
+            <Text style={styles.name}>{profile.first_name}</Text>
+        </TouchableOpacity>
     )
 }
+
+const styles = StyleSheet.create({
+    card: {
+        alignItems: "center",
+        margin: 10,
+        width: 120
+    },
+
+    defaultAvatar: {
+        width: 100,
+        height: 100,
+        borderRadius: 8,
+        backgroundColor: "#2196F3",
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth: 3,
+        borderColor: "transparent"
+    },
+
+    avatarText: {
+        fontSize: 48,
+        color: "#fff",
+        fontWeight: "600",
+    },
+
+    name: {
+        fontSize: 16,
+        color: "#808080",
+        textAlign: "center",
+    }
+});
 
--- app/profile-select.tsx
+++ app/profile-select.tsx
@@ -1,11 +1,17 @@
 import { useAuth } from '@/context/auth-context';
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import ProfileCard from './components/ProfileCard';
 
 export default function ProfileSelectScreen () {
     const {profiles} = useAuth();
+
+    function onPress(id: string) {
+
+    }
+
     return (
-        <View>
+        <View style={styles.profilesContainer}>
+            <Text>Who is using Fridge Mate?</Text>
             {
                 profiles.map((profile) => (
                     <ProfileCard key={profile.profile_id} profile={profile}/>
@@ -14,3 +20,11 @@ export default function ProfileSelectScreen () {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    profilesContainer: {
+        flex: 1,
+        justifyContent: "center",
+    }
+})
+
