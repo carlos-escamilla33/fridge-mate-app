e6006052c5fa821ce4338744bc39bc5a080594bb
Carlos Escamilla <carlosaescamilla3@gmail.com>
2026-01-13 20:21:51 -0800

    setting up tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import AuthProvider from "@/context/auth-context";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <AuthProvider>
+        <Stack>
+          
+        </Stack>
+    </AuthProvider>
+  );
+}
