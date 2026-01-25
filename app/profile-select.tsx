a10f5341712431d7d167031e9c623a279b32d5b1
Carlos Escamilla <carlosaescamilla3@gmail.com>
2026-01-24 20:12:48 -0800

    updated routing from profile selection to tabs homescreen

--- app/profile-select.tsx
+++ app/profile-select.tsx
@@ -5,15 +5,16 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import ProfileCard from "./components/ProfileCard";
 
 export default function ProfileSelectScreen() {
-  const { profiles, setCurrentProfile, currentProfile, getAllProfiles } = useAuth();
+  const { profiles, setCurrentProfile, currentProfile, getAllProfiles } =
+    useAuth();
   const router = useRouter();
 
   useFocusEffect(
     useCallback(() => {
-        getAllProfiles();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-  )
+      getAllProfiles();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []),
+  );
 
   function handleProfilePress(profile: Profile): void {
     setCurrentProfile(profile);
@@ -21,7 +22,7 @@ export default function ProfileSelectScreen() {
   }
 
   function handleAddProfilePress() {
-    router.replace("./profile-create");
+    router.replace("/(tabs)");
   }
 
   return (
@@ -29,73 +30,77 @@ export default function ProfileSelectScreen() {
       <Text style={styles.title}>Who is using Fridge Mate?</Text>
       <View style={styles.profileContainer}>
         {profiles.map((profile) => (
-          <ProfileCard key={profile.profile_id} profile={profile} handleProfilePress={handleProfilePress}/>
+          <ProfileCard
+            key={profile.profile_id}
+            profile={profile}
+            handleProfilePress={handleProfilePress}
+          />
         ))}
-        <TouchableOpacity style={styles.addBtnCard} onPress={handleAddProfilePress}>
-                <View style={styles.addAvatar}>
-                    <Text style={styles.symbolText}>
-                        +
-                    </Text>
-                </View>
-                <Text style={styles.addText}>Add Profile</Text>
+        <TouchableOpacity
+          style={styles.addBtnCard}
+          onPress={handleAddProfilePress}
+        >
+          <View style={styles.addAvatar}>
+            <Text style={styles.symbolText}>+</Text>
+          </View>
+          <Text style={styles.addText}>Add Profile</Text>
         </TouchableOpacity>
-        <View>
-        </View>
+        <View></View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#141414",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 20
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#141414",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
 
-    title: {
-        textAlign: "center",
-        fontSize: 32,
-        color: "#fff",
-        marginBottom: 40,
-        fontWeight: "500"
-    },
+  title: {
+    textAlign: "center",
+    fontSize: 32,
+    color: "#fff",
+    marginBottom: 40,
+    fontWeight: "500",
+  },
 
-    profileContainer: {
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        gap: 20
-    },
+  profileContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    gap: 20,
+  },
 
-    addBtnCard: {
-        alignItems: "center",
-        margin: 10,
-        width: 120
-    },
+  addBtnCard: {
+    alignItems: "center",
+    margin: 10,
+    width: 120,
+  },
 
-    addAvatar: {
-        width: 100,
-        height: 100,
-        borderRadius: 8,
-        backgroundColor: "#2196F3",
-        justifyContent: "center",
-        alignItems: "center",
-        borderWidth: 3,
-        borderColor: "transparent"
-    },
+  addAvatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 8,
+    backgroundColor: "#2196F3",
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 3,
+    borderColor: "transparent",
+  },
 
-    symbolText: {
-        fontSize: 48,
-        color: "#fff",
-        fontWeight: "600",
-    },
+  symbolText: {
+    fontSize: 48,
+    color: "#fff",
+    fontWeight: "600",
+  },
 
-    addText: {
-        fontSize: 16,
-        color: "#808080",
-        textAlign: "center",
-    }
+  addText: {
+    fontSize: 16,
+    color: "#808080",
+    textAlign: "center",
+  },
 });
