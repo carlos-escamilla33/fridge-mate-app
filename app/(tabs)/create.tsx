bbd911f0cde4f137ee8a7e839761a5056747af8e
Carlos Escamilla <carlosaescamilla3@gmail.com>
2026-01-24 19:15:58 -0800

    fixed date picker input sizing

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,9 +1,73 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { KeyboardAvoidingView, Platform, StyleSheet, View } from "react-native";
+import { TextInput } from "react-native-paper";
+import { DatePickerInput } from "react-native-paper-dates";
 
 export default function CreateScreen() {
-    return (
+  const [itemName, setItemName] = useState<string>("");
+  const [inputDate, setInputDate] = useState<Date | undefined>(undefined);
+
+  function handleInputChange() {
+    console.log("updating inputs");
+  }
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <View style={styles.content}>
+        <TextInput
+          label="Item Name"
+          autoCapitalize="none"
+          placeholder="Apple"
+          mode="outlined"
+          value={itemName}
+          onChangeText={setItemName}
+        />
+        <TextInput
+          label="Ripeness Level"
+          placeholder="Level 1"
+          mode="outlined"
+        />
         <View>
-            <Text>Tab [Create]</Text>
+          <DatePickerInput
+            locale="en"
+            label="Expiration Date"
+            mode="outlined"
+            animationType="fade"
+            value={inputDate}
+            onChange={setInputDate}
+            inputMode="start"
+            // style={styles.dateInput}
+          />
         </View>
-    )
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 16,
+  },
+  //   title: {
+  //     textAlign: "center",
+  //     marginBottom: 24,
+  //   },
+  //   input: {
+  //     marginBottom: 16,
+  //   },
+  //   button: {
+  //     textAlign: "center",
+  //     marginTop: 8,
+  //   },
+  //   switchModeButton: {
+  //     marginTop: 16,
+  //   },
+});
