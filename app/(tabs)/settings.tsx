2ed1926d21bc64924ac1239b1738ed044650a1ba
Carlos Escamilla <carlosaescamilla3@gmail.com>
2026-02-03 12:31:33 -0800

    added icons to settings tab

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,9 +1,34 @@
-import { Text, View } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function SettingsScreen() {
-    return (
-        <View>
-            <Text>Tab [Settings Screen]</Text>
-        </View>
-    )
+  return (
+    <View style={styles.settingsContainer}>
+      <View style={styles.iconsContainer}>
+        <Text style={styles.textStyling}>Sign Out</Text>
+        <MaterialIcons name="logout" size={24} color="black" />
+      </View>
+      <View style={styles.iconsContainer}>
+        <Text style={styles.textStyling}>Switch Accounts</Text>
+        <AntDesign name="user-switch" size={24} color="black" />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  settingsContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconsContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  textStyling: {
+    padding: 10,
+  },
+});
