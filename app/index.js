0d3d19aad4256fed81646a839708fad72d4265ab
Carlos Escamilla <carlosaescamilla3@gmail.com>
2026-05-03 21:23:50 -0700

    feat: working on profiles screen

--- app/index.js
+++ app/index.js
@@ -35,7 +35,7 @@ export default function SplashScreen() {
 
   useEffect(() => {
     if (!isLoading && animationDone) {
-      router.replace(user ? "/(app)/profiles" : "/(app)/profiles"); // change this pack to /(app)/sign-in, after profiles screen is finished
+      router.replace(user ? "/(app)/profiles" : "/(app)/profiles"); // change back to sign-in screen are profiles development
     }
   }, [isLoading, animationDone]);
 
