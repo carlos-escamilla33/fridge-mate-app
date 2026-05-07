05700af2e844aeae1246b5b7868f22e2e6eca66e
Carlos Escamilla <carlosaescamilla3@gmail.com>
2026-05-06 21:30:18 -0700

    feat: added useEffect in profiles screen

--- app/(app)/profiles.js
+++ app/(app)/profiles.js
@@ -15,4 +15,27 @@ import Colors from '../../constants/colors';
 export default function ProfilesScreen() {
     const {user} = useAuth
     const fadeAmin = useRef(new Animated.Value(0)).current;
+    const slideAnim = useRef(new Animated.Value(30)).current;
+
+    // Fix this to come from backend later on
+    // ONLY FOR TESTING PURPOSES
+
+    const [members] = useState([
+      {id: user?.id ?? "1", name: user?.name ?? "You", isAdmin: true},
+    ])
+
+    useEffect(() => {
+      Animated.parallel([
+        Animated.timing(fadeAmin, {
+          toValue: 1,
+          duration: 500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(slideAnim, {
+          toValue: 0,
+          duration: 450,
+          useNativeDriver: true,
+        }),
+      ]).start()
+    }, []);
 }
